bdfde726656df824e421de2afd06b0aae9aef831
Joe Lombrozo <joe@djeebus.net>
2019-07-05 18:19:38 -0400

    more tweaks

--- keyboards/ergodox_ez/keymaps/djeebus/config.h
+++ keyboards/ergodox_ez/keymaps/djeebus/config.h
@@ -24,5 +24,5 @@
 // #undef IGNORE_MOD_TAP_INTERRUPT
 
 // #define TAPPING_TOGGLE  1
-#define TAPPING_TERM    200
+// #define TAPPING_TERM    150
 // #define IGNORE_MOD_TAP_INTERRUPT
--- keyboards/ergodox_ez/keymaps/djeebus/keymap.c
+++ keyboards/ergodox_ez/keymaps/djeebus/keymap.c
@@ -38,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ergodox(
         // left hand
         KC_ESCAPE,          KC_1,           KC_2,       KC_3,    KC_4,    KC_5,    KC_6,
-        KC_TAB,             KC_Q,           KC_W,       KC_E,    KC_R,    KC_T,    KC_HOME,
+        KC_TAB,             KC_Q,           KC_W,       KC_E,    KC_R,    KC_T,    KC_VOLU,
         KC_CAPSLOCK,        KC_A,           KC_S,       KC_D,    KC_F,    KC_G,
-        KC_LSFT,            KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_END,
+        KC_LSFT,            KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_VOLD,
         KC_LCTRL, MO(MDIA),  MO(SYMB),  KC_LALT,    KC_SPACE,
                                                                 KC_TRNS, KC_TRNS,
                                                                                 KC_PGUP,
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS,   KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,  KC_LPRN, KC_RPRN, KC_GRV,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
-        RESET,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        RESET,    EPRM, KC_TRNS, KC_TRNS, KC_TRNS,
                                                     RGB_MOD, KC_TRNS,
                                                                 KC_F3,
                                             RGB_VAD, RGB_VAI, KC_TRNS,
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_END,
         KC_TRNS, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_PGUP,
         KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_PGDOWN,
-        KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+        KC_0, KC_DOT,  KC_TRNS,    KC_EQL,  KC_TRNS,
         RGB_TOG, RGB_SLD,
         KC_TRNS,
         KC_RCTRL, RGB_HUD, RGB_HUI
@@ -89,11 +89,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 KC_TRNS,
                                             KC_WWW_BACK, KC_TRNS, KC_WWW_FORWARD,
         // right hand
-        KC_F7, KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12, KC_PGUP,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDOWN,
-                KC_TRNS, KC_RCTRL, KC_RSHIFT, KC_RALT, KC_TRNS, KC_HOME,
-        KC_TRNS, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_UP, KC_END,
-                            KC_VOLU, KC_VOLD, KC_LEFT, KC_DOWN, KC_RIGHT,
+        KC_F7, KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                KC_TRNS, KC_RCTRL, KC_RSHIFT, KC_RALT, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS,
         KC_TRNS, KC_RGUI, KC_WBAK
