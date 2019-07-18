4d9852a6954c30ebb611075515b188c8ee776d26
Joe Lombrozo <joe@djeebus.net>
2019-07-18 12:59:54 -0400

    add right ctrl/alt

--- keyboards/ergodox_ez/keymaps/djeebus/keymap.c
+++ keyboards/ergodox_ez/keymaps/djeebus/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PGUP,     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLASH,
         KC_H,         KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_QUOT,
         KC_PGDOWN, KC_N,    KC_M,    KC_COMM, KC_DOT,            KC_SLASH, KC_RSFT,
-        KC_SPACE,        KC_TRNS, KC_LBRC, KC_RBRC, MO(SYMB),
+        KC_SPACE,        KC_TRNS, KC_RALT, KC_RCTRL, MO(SYMB),
 
         KC_TRNS, KC_TRNS,
         KC_TRNS,
