80c9b2b02f3c8a876a7f0987eac073fb412f3d7f
Joe Lombrozo <joe@djeebus.net>
2019-10-29 16:21:00 -0400

    Caps == ctrl+shift when held

--- keyboards/ergodox_ez/keymaps/djeebus/keymap.c
+++ keyboards/ergodox_ez/keymaps/djeebus/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESCAPE,          KC_1,           KC_2,       KC_3,    KC_4,    KC_5,    KC_6,
         KC_TAB,             KC_Q,           KC_W,       KC_E,    KC_R,    KC_T,    KC_VOLU,
-        KC_CAPSLOCK,        KC_A,           KC_S,       KC_D,    KC_F,    KC_G,
+        MT(MOD_LCTL | MOD_LSFT, KC_CAPSLOCK),        KC_A,           KC_S,       KC_D,    KC_F,    KC_G,
         KC_LSFT,            KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_VOLD,
         KC_LCTRL, MO(MDIA),  MO(SYMB),  KC_LALT,    KC_SPACE,
                                                                 KC_TRNS, KC_TRNS,
