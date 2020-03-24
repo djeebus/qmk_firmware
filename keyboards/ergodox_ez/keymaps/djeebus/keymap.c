b04bcbabaf370779da3e773e654a81ef310f3160
Joe Lombrozo <joe@djeebus.net>
2020-03-24 14:33:11 -0400

    swap period

--- keyboards/ergodox_ez/keymaps/djeebus/keymap.c
+++ keyboards/ergodox_ez/keymaps/djeebus/keymap.c
@@ -59,21 +59,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [SYMB] = LAYOUT_ergodox(
         // left hand
-        KC_TRNS,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
-        KC_TRNS, KC_TRNS, KC_TRNS,   KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,  KC_LPRN, KC_RPRN, KC_GRV,
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_GRV,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
-        RESET,    EPRM, KC_TRNS, KC_TRNS, KC_TRNS,
+        RESET,   EPRM,    KC_TRNS, KC_TRNS, KC_TRNS,
                                                     RGB_MOD, KC_TRNS,
                                                                 KC_F3,
                                             RGB_VAD, RGB_VAI, KC_TRNS,
         // right hand
-        KC_F7, KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_HOME,
-        KC_TRNS, KC_TRNS,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_END,
-        KC_TRNS, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_PGUP,
-        KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_PGDOWN,
-        KC_0, KC_DOT,  KC_TRNS,    KC_EQL,  KC_TRNS,
-        RGB_TOG, RGB_SLD,
+        KC_F7,      KC_F8,   KC_F9, KC_F10,  KC_F11,  KC_F12,  KC_HOME,
+        KC_TRNS,    KC_TRNS, KC_7,  KC_8,    KC_9,    KC_ASTR, KC_END,
+                    KC_TRNS, KC_4,  KC_5,    KC_6,    KC_PLUS, KC_PGUP,
+        KC_TRNS,    KC_TRNS, KC_1,  KC_2,    KC_3,    KC_BSLS, KC_PGDOWN,
+                             KC_0,  KC_TRNS, KC_DOT,  KC_EQL,  KC_TRNS,
+        RGB_TOG,    RGB_SLD,
         KC_TRNS,
         KC_RCTRL, RGB_HUD, RGB_HUI
     ),
