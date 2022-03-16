dd21682081af04be6f7cf7b4747d470db0656198
Joe Lombrozo <joe@djeebus.net>
2022-03-15 20:15:02 -0400

    clean up for the last upgrade

--- keyboards/ergodox_ez/keymaps/djeebus/keymap.c
+++ keyboards/ergodox_ez/keymaps/djeebus/keymap.c
@@ -13,22 +13,21 @@ enum custom_keycodes {
 #else
     VRSN = SAFE_RANGE,
 #endif
-    EPRM
 };
 
-#define MAGIC_TAB MT(MOD_LCTL|MOD_LSFT,KC_CAPSLOCK)
+#define MAGIC_CAPS MT(MOD_LCTL|MOD_LSFT,KC_CAPSLOCK)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox_pretty(
-    KC_ESCAPE,      KC_1,       KC_2,       KC_3,    KC_4,    KC_5,    KC_6,            KC_6,       KC_7,       KC_8,     KC_9,    KC_0,    KC_MINUS,         KC_EQUAL,
-    KC_TAB,         KC_Q,       KC_W,       KC_E,    KC_R,    KC_T,    KC_PGUP,         KC_PSCREEN,    KC_Y,       KC_U,     KC_I,    KC_O,    KC_P,             KC_BSLASH,
-    MAGIC_TAB,      KC_A,       KC_S,       KC_D,    KC_F,    KC_G,                                 KC_H,       KC_J,     KC_K,    KC_L,    LT(MDIA,KC_SCLN), KC_QUOT,
-    KC_LSFT,        KC_Z,       KC_X,       KC_C,    KC_V,    KC_B,    KC_PGDOWN,         KC_PAUSE,  KC_N,       KC_M,     KC_COMM, KC_DOT,  KC_SLASH,         KC_RSFT,
-    KC_LCTRL,       MO(MDIA),   MO(SYMB),   KC_LALT, KC_SPACE,                                                  KC_SPACE, KC_TRNS, KC_RALT, KC_RCTRL,         MO(SYMB),
-                                                        KC_MPRV,    KC_MNXT,            KC_TRNS,    KC_TRNS,
-                                                                    KC_VOLU,            KC_TRNS,
-                                            KC_BSPACE, KC_DELETE,  KC_VOLD,           MO(MDIA),   KC_LGUI,    KC_ENTER
+    KC_GESC,        KC_1,       KC_2,       KC_3,    KC_4,      KC_5,      KC_6,            KC_6,       KC_7,       KC_8,     KC_9,    KC_0,    KC_MINUS,         KC_EQUAL,
+    KC_TAB,         KC_Q,       KC_W,       KC_E,    KC_R,      KC_T,      KC_PGUP,         KC_PSCREEN, KC_Y,       KC_U,     KC_I,    KC_O,    KC_P,             KC_BSLASH,
+    MAGIC_CAPS,     KC_A,       KC_S,       KC_D,    KC_F,      KC_G,                                   KC_H,       KC_J,     KC_K,    KC_L,    LT(MDIA,KC_SCLN), KC_QUOT,
+    KC_LSFT,        KC_Z,       KC_X,       KC_C,    KC_V,      KC_B,      KC_PGDOWN,       KC_PAUSE,   KC_N,       KC_M,     KC_COMM, KC_DOT,  KC_SLASH,         KC_RSFT,
+    KC_LCTRL,       MO(MDIA),   MO(SYMB),   KC_LALT, KC_SPACE,                                                      KC_SPACE, KC_TRNS, KC_RALT, KC_RCTRL,         MO(SYMB),
+                                                                KC_MPRV,   KC_MNXT,         KC_TRNS,    KC_TRNS,
+                                                                           KC_VOLU,         KC_TRNS,
+                                                     KC_BSPACE, KC_DELETE, KC_VOLD,         MO(MDIA),   KC_LGUI,    KC_ENTER
 ),
 
 [SYMB] = LAYOUT_ergodox_pretty(
@@ -36,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS,                  KC_TRNS,    KC_TRNS, KC_7,  KC_8,    KC_9,    KC_ASTR, KC_END,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_GRV,                                        KC_TRNS, KC_4,  KC_5,    KC_6,    KC_PLUS, KC_PGUP,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,                  KC_TRNS,    KC_TRNS, KC_1,  KC_2,    KC_3,    KC_BSLS, KC_PGDOWN,
-    RESET,   EPRM,    KC_TRNS, KC_TRNS, KC_TRNS,                                                         KC_0,  KC_TRNS, KC_DOT,  KC_EQL,  KC_TRNS,
+    RESET,   EEPROM_RESET,    KC_TRNS, KC_TRNS, KC_TRNS,                                                         KC_0,  KC_TRNS, KC_DOT,  KC_EQL,  KC_TRNS,
                                                 RGB_MOD, KC_TRNS,                   RGB_TOG,    KC_TRNS,
                                                             KC_F3,                  KC_TRNS,
                                         RGB_VAD, RGB_VAI, KC_TRNS,                  KC_RCTRL, RGB_HUD, RGB_HUI
@@ -59,9 +58,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
-            case EPRM:
-                eeconfig_init();
-                return false;
             case VRSN:
                 SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
                 return false;
