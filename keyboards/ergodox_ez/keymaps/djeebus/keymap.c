a365c67a59891e3deb53222c549c74a1a8bcb8e0
Joe Lombrozo <joe@djeebus.net>
2026-06-29 10:02:34 -0700

    tweak the keymap

--- keyboards/ergodox_ez/keymaps/djeebus/keymap.c
+++ keyboards/ergodox_ez/keymaps/djeebus/keymap.c
@@ -20,10 +20,10 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox_pretty(
-    QK_GESC,        KC_1,       KC_2,       KC_3,    KC_4,      KC_5,      KC_6,            KC_6,       KC_7,       KC_8,     KC_9,    KC_0,    KC_MINUS,         KC_EQUAL,
+    QK_GESC,        KC_1,       KC_2,       KC_3,    KC_4,      KC_5,      KC_EQUAL,        KC_MINUS,   KC_6,       KC_7,     KC_8,      KC_9,    KC_0,    KC_MINUS,
     KC_TAB,         KC_Q,       KC_W,       KC_E,    KC_R,      KC_T,      KC_PGUP,         KC_PSCR,    KC_Y,       KC_U,     KC_I,    KC_O,    KC_P,             KC_BSLS,
     MAGIC_CAPS,     KC_A,       KC_S,       KC_D,    KC_F,      KC_G,                                   KC_H,       KC_J,     KC_K,    KC_L,    LT(MDIA,KC_SCLN), KC_QUOT,
-    KC_LSFT,        KC_Z,       KC_X,       KC_C,    KC_V,      KC_B,      KC_PGDN,         KC_PAUSE,   KC_N,       KC_M,     KC_COMM, KC_DOT,  KC_SLASH,         KC_RSFT,
+    MT(MOD_LSFT,KC_EQUAL), KC_Z, KC_X,       KC_C,    KC_V,      KC_B,      KC_PGDN,        KC_PAUSE,   KC_N,       KC_M,     KC_COMM, KC_DOT,  KC_SLASH,         MT(MOD_RSFT,KC_MINUS),
     MO(MDIA),       KC_LCTL,    MO(SYMB),   KC_LALT, KC_SPACE,                                                      KC_SPACE, KC_TRNS, KC_RALT, KC_RCTL,         MO(SYMB),
                                                                 KC_MPRV,   KC_MNXT,         KC_TRNS,    KC_TRNS,
                                                                            KC_VOLU,         KC_TRNS,
