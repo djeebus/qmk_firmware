4aa2ad2da3f80340952e7e1074b6910d415221ac
Joe Lombrozo <joe@djeebus.net>
2026-06-29 15:21:23 -0700

    tweaks

--- keyboards/ergodox_ez/keymaps/djeebus/keymap.c
+++ keyboards/ergodox_ez/keymaps/djeebus/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,       KC_W,       KC_E,    KC_R,      KC_T,      KC_PGUP,         KC_PSCR,    KC_Y,       KC_U,     KC_I,    KC_O,    KC_P,             KC_BSLS,
     MAGIC_CAPS,     KC_A,       KC_S,       KC_D,    KC_F,      KC_G,                                   KC_H,       KC_J,     KC_K,    KC_L,    LT(MDIA,KC_SCLN), KC_QUOT,
     MT(MOD_LSFT,KC_EQUAL), KC_Z, KC_X,       KC_C,    KC_V,      KC_B,      KC_PGDN,        KC_PAUSE,   KC_N,       KC_M,     KC_COMM, KC_DOT,  KC_SLASH,         MT(MOD_RSFT,KC_MINUS),
-    MO(MDIA),       TD(TD_LCBR), TD(TD_RCBR), KC_LALT, KC_SPACE,                                                    KC_SPACE, KC_TRNS, MT(MOD_RALT,KC_LBRC), MT(MOD_RCTL,KC_RBRC), MO(SYMB),
+    MO(MDIA),       TD(TD_LCBR), TD(TD_RCBR), KC_GRV, KC_SPACE,                                                    KC_SPACE, KC_TRNS, MT(MOD_RALT,KC_LBRC), MT(MOD_RCTL,KC_RBRC), MO(SYMB),
                                                                 KC_MPRV,   KC_MNXT,         KC_TRNS,    KC_TRNS,
                                                                            KC_VOLU,         KC_TRNS,
                                                      KC_BSPC,   KC_DELETE, KC_VOLD,         MO(MDIA),   KC_LGUI,    KC_ENTER
@@ -112,6 +112,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
     }
     return true;
+}
+
+// Immediately resolve the Shift mod-taps to "hold" when another key is pressed,
+// so fast rolls like Shift+I don't emit the tap symbol ("=i" / "-i").
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MT(MOD_LSFT, KC_EQUAL):
+        case MT(MOD_RSFT, KC_MINUS):
+            return true;
+        default:
+            return false;
+    }
 }
 
 // Runs just one time when the keyboard initializes.
